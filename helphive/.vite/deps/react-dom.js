cced76654f2dd257b9ef17ea4cab02f4b3dd5563
mekam27 <mekam@purdue.edu>
2025-01-25 20:03:05 -0500

    Additional changes made

---
+++ helphive/.vite/deps/react-dom.js
@@ -0,0 +1,6 @@
+import {
+  require_react_dom
+} from "./chunk-VJJ6GTOK.js";
+import "./chunk-ZK7KK5L5.js";
+import "./chunk-DC5AMYBS.js";
+export default require_react_dom();
---
+++ helphive/.vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-ZK7KK5L5.js";
+import "./chunk-DC5AMYBS.js";
+export default require_react();
